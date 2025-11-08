86b364f5951436502689f4ebf73470b11798bd36
Fedora Anestasia <115520786+highlysoul@users.noreply.github.com>
2025-11-08 14:24:07 +0700

    Commit No. 1 – Building Jetpack Compose UI
    Building Jetpack Compose U

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -48,7 +47,10 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    //implementation(libs.androidx.compose.material3)
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.ui:ui-text")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
